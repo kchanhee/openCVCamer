053b8723269e16651df8e4768e5d968c6619c02a
BallerChan <chan@baller.io>
2017-04-16 12:42:09 -0700

    Initial Commit

---
+++ OpenCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCamera
+//
+//  Created by Chan on 4/16/17.
+//  Copyright © 2017 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCamera
+//
+//  Created by Chan on 4/16/17.
+//  Copyright © 2017 Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
